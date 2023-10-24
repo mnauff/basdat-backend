3f9935373ec2a5941bae9ed2b425d3155530fdf6
mnauff <mnaufalrafianto@gmail.com>
2023-10-24 20:51:51 +0700

    feat(formatter): add commitlint and eslint

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
-import router from "./router/index.js";
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import router from './router/index.js'
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 3000
 
 // Middleware to parse JSON requests
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
 // Define your routes here
-app.use("/", router);
+app.use('/', router)
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+    console.log(`Server is running on port ${port}`)
+})
--- src/utils/seed.js
+++ src/utils/seed.js
@@ -1,27 +1,27 @@
-import prisma from "./prisma.js";
+import prisma from './prisma.js'
 
 async function main() {
-  try {
-    const studentData = await prisma.student.create({
-      data: {
-        name: "Muhammad Naufal Rafianto",
-        studentId: "5024211042",
-        email: "mnaufalrafianto@gmail.com",
-        grade: 98.5,
-      },
-    });
-    console.log({ studentData });
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        const studentData = await prisma.student.create({
+            data: {
+                name: 'Muhammad Naufal Rafianto',
+                studentId: '5024211042',
+                email: 'mnaufalrafianto@gmail.com',
+                grade: 98.5,
+            },
+        })
+        console.log({ studentData })
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
