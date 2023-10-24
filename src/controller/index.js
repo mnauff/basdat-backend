2ce46dbea2e22184024b9edd52c8dd8d0f2d3b0b
mnauff <mnaufalrafianto@gmail.com>
2023-10-24 21:33:05 +0700

    feat: add github actions

--- src/controller/index.js
+++ src/controller/index.js
@@ -3,6 +3,9 @@ import { createAccount, getAllAccount } from '../controller/user.controller.js'
 
 const router = express.Router()
 
+router.get('/', (req, res) => {
+    res.json({ msg: 'Hello' })
+})
 router.get('/account', getAllAccount)
 router.post('/account', createAccount)
 export default router
--- src/index.js
+++ src/index.js
@@ -4,15 +4,14 @@ import bodyParser from 'body-parser'
 import router from './router/index.js'
 
 const app = express()
-const port = process.env.PORT || 3000
 
 // Middleware to parse JSON requests
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
 // Define your routes here
-app.use('/', router)
+app.use('/api/v1', router)
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
+app.listen(8888, () => {
+    console.log(`Server is running on port ${8888}`)
 })
