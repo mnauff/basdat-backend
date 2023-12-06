e597b64655aa51f9aa664cc0c521aeccf1a61411
Naufal Rafianto <mnaufalrafianto@gmail.com>
2023-12-06 14:30:46 +0700

    1 praktikum (#5)
    * feat: add new students not fix
    
    * feat: endpoint praktikum
    
    ---------
    
    Co-authored-by: Braceskabane <allzoldyck31@gmail.com>

--- src/practicum/practicum.controller.js
+++ src/practicum/practicum.controller.js
@@ -1,9 +1,12 @@
 import express from 'express'
-import { createPracticum, getAllPracticum } from './practicum.service.js'
+import { createPracticum, getAllPracticum, getById, patchById, delById } from './practicum.service.js'
 
 const router = express.Router()
 
 router.get('/', getAllPracticum)
 router.post('/', createPracticum)
+router.get('/:id', getById)
+router.patch('/:id', patchById)
+router.delete('/:id', delById)
 
 export { router as practicum }
--- src/practicum/practicum.service.js
+++ src/practicum/practicum.service.js
@@ -2,6 +2,7 @@ import { hashPw } from '../../utils/hash.js'
 import prisma from '../../utils/prisma.js'
 import { v4 as uuidv4 } from 'uuid'
 
+/** Melihat berdasarkan jenis praktikum  */
 export const getAllPracticum = async (req, res) => {
     try {
         const practicums = await prisma.practicum.findMany()
@@ -10,6 +11,7 @@ export const getAllPracticum = async (req, res) => {
                 message: 'No practicums found in the database.',
             })
         }
+        res.json(practicums)
     } catch (error) {
         res.status(500).json({
             message: 'Something went wrong',
@@ -17,49 +19,110 @@ export const getAllPracticum = async (req, res) => {
     }
 }
 
-export const createPracticum = async (req, res) => {
+/** Melihat berdasarkan Id  */
+export const getById = async (req, res) => {
     try {
-        const { accountId, password, role } = req.body
-        const hashedPassword = await hashPw(password) // Assuming 'hashPw' is an asynchronous function
+        const { id } = req.params
         const createId = uuidv4()
 
-        const isStudent = await prisma.student.findFirst({
+        const existingPracticum = await prisma.practicum.findUnique({
             where: {
-                studentId: accountId,
+                practicumId: id,
+                /** sama name: name, dengan name, */
             },
         })
 
-        const existingAccount = await prisma.account.findFirst({
-            where: {
-                accountId: accountId,
-            },
-        })
-
-        if (role === 'STUDENT') {
-            if (!isStudent) {
-                return res.status(400).json({ message: 'Student ID is not found!' })
-            }
+        if (!existingPracticum) {
+            return res.status(404).json({ message: 'Not Found!' })
         }
+        /**kalo berhasil menampilkan */
+        return res.status(200).json(existingPracticum)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ message: 'Internal server error' })
+    }
+}
 
-        if (existingAccount) {
-            return res.status(400).json({ message: 'Account already exists!' })
+/** Mengganti nilai item praktikum */
+export const patchById = async (req, res) => {
+    try {
+        /**Req dari parameter id jadi parameter (12c8124e-cf30-45a4-9e24-18ff437e56d1) */
+        const { id } = req.params
+        /**Req dari Body */
+        const { name, place, time } = req.body
+        const createId = uuidv4()
+
+        const existingPracticum = await prisma.practicum.update({
+            where: {
+                practicumId: id,
+                /** sama name: name, dengan name, */
+            },
+            data: {
+                name: name,
+                place: place,
+                time: time,
+            },
+        })
+
+        if (!existingPracticum) {
+            return res.status(404).json({ message: 'Not Found!' })
+        }
+        /**kalo berhasil menampilkan */
+        return res.status(200).json(existingPracticum)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ message: 'Internal server error' })
+    }
+}
+
+/** Menghapus Id Praktikum (Jenis Praktikum) */
+export const delById = async (req, res) => {
+    try {
+        /**Req dari parameter id jadi parameter (12c8124e-cf30-45a4-9e24-18ff437e56d1) */
+        const { id } = req.params
+
+        const existingPracticum = await prisma.practicum.delete({
+            where: {
+                practicumId: id,
+                /** sama name: name, dengan name, */
+            },
+        })
+
+        if (!existingPracticum) {
+            return res.status(404).json({ message: 'Not Found!' })
+        }
+        /**kalo berhasil menampilkan */
+        return res.status(200).json({ message: `praktikum with ${id} has been deleted` })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ message: 'Internal server error' })
+    }
+}
+
+/** Membuat data praktikum  */
+export const createPracticum = async (req, res) => {
+    try {
+        const { name, place, time } = req.body
+        const createId = uuidv4()
+
+        const existingPracticum = await prisma.practicum.findFirst({
+            where: {
+                name: name,
+                /** sama name: name, dengan name, */
+            },
+        })
+
+        if (existingPracticum) {
+            return res.status(400).json({ message: 'Practicum already exists!' })
         }
 
         // Create the account
-        await prisma.account.create({
+        await prisma.practicum.create({
             data: {
-                id: createId,
-                accountId,
-                password: hashedPassword,
-                role: role,
-            },
-        })
-
-        // Update the student to link it to the created account
-        await prisma.student.update({
-            where: { studentId: accountId },
-            data: {
-                accountId: createId, // Assuming you want to link the student to the account using 'accountId'
+                practicumId: createId,
+                name: name,
+                place: place,
+                time: time,
             },
         })
 
